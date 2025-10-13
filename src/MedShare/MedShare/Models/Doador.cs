2137fdbfbbf88ae66392924a577df2ed5a9aa02d
Jefferson Torres <jefferson.torrreis@gmail.com>
2025-10-13 15:23:21 -0300

    Doador_CRUD

---
+++ src/MedShare/MedShare/Models/Doador.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedShare.Models {
+
+    [Table("Doadors")]
+    public class Doador 
+    {
+        [Key]
+        public int DoadorId { get; set; }
+
+        [Required(ErrorMessage ="Obrigatorio informar nome!")]
+        public string DoadorName { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar cpf!")]
+        public string DoadorCpf { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar email!")]
+        public string DoadorEmail{ get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar data de nascimento!")]
+        public DateOnly DoadorDataNascimento { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar senha!")]
+        public string DoadorSenha { get; set; }
+    }
+}
