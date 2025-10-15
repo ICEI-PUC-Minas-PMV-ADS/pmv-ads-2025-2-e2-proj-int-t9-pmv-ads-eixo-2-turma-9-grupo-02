8a216adfc1638842dba906de452681c54de45d15
Jefferson Torres <jefferson.torrreis@gmail.com>
2025-10-15 01:11:26 -0300

    CRUD_Login&Authentication&&Autorization

---
+++ src/MedShare/MedShare/MedShare/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace MedShare.Models {
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
