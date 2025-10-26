98879bac94c62913187e83608fde11960c411cb4
estevaoSGA <estevao.santos@sga.pucminas.br>
2025-10-26 15:24:15 -0300

    Mudanças no sistema de notificação e inclusão de conta teste de doador no DB

--- src/MedShare/MedShare/MedShare/Models/Notificacao.cs
+++ src/MedShare/MedShare/MedShare/Models/Notificacao.cs
@@ -2,9 +2,12 @@
 {
     public class Notificacao
     {
+        public int Id { get; set; }
+        public string UsuarioId { get; set; }
         public string Titulo { get; set; }
         public string Mensagem { get; set; }
         public bool Lida { get; set; }
         public DateTime Horario { get; set; }
+        public string Tipo { get; set; } // "sucesso", "erro", "aviso", "informação"
     }
 }
